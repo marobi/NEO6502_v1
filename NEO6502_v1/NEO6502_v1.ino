17ffd2eda93ecf6abe567df874c28cdb04735144
Rien <rien@marobi.com>
2024-01-13 14:07:01 +0100

    Fixes

--- NEO6502_v1/NEO6502_v1.ino
+++ NEO6502_v1/NEO6502_v1.ino
@@ -17,6 +17,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  Author:	Rien Matthijsse
 */
 #include "NEO6502.h"
+#include "Adafruit_TinyUSB.h"
 
 #include "version.h"
 
